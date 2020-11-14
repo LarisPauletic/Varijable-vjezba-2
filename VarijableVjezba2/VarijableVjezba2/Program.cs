5f0ead93ff0e3ccd38c35d9051c0359f92f622d9
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2020-11-14 19:14:00 +0100

    Add files via upload

---
+++ VarijableVjezba2/VarijableVjezba2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarijableVjezba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj = 100;
+            int broj1 = -1000;
+            char slovo = "a";
+            float broj2= 3.123;
+            float broj3 = 3.123456789;
+            string rijec = "true";
+            bool rijec1 = true;
+        }
+    }
+}
